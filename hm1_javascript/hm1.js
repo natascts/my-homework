ce9f11d8053b4fefb378d3a944b6b3a8012bda30
Nataliia Shcherbets <shcherbets2001@gmail.com>
2025-02-08 20:48:43 +0200

    Add files via upload

---
+++ hm1_javascript/hm1.js
@@ -0,0 +1,35 @@
+var v1 = 55
+var v2 = 'Hello'
+var v3 = 'Are you sure?'
+var v4 = true
+var v5 = 'JavaScript'
+let l1 = 'False'
+let l2 = 'This page can use your confidential information'
+let l3 = 'English'
+let l4 = 77
+let l5 = 'Cat'
+const c1 = 55
+const c2 = 'Verb'
+const c3 = false
+const c4 = 44
+const c5 = 'Confirm'
+console.log(v1)
+console.log(v4)
+console.log(c3)
+document.write(v5)
+document.write('<br>')
+document.write(l3)
+document.write('<br>')
+document.write(c4)
+alert(v2)
+alert(l1)
+alert(c5)
+confirm(l2)
+confirm(v3)
+confirm(l5)
+prompt(l4)
+prompt(c1)
+prompt(c2)
+document.write('<hr>')
+document.write('<p><b>"var"</b>-застарілий спосіб оголошення змінних, <b>"let"</b> та <b>"const"</b> - сучасні та використовуються зараз. <b>"var"</b> та <b>"let"</b> можуть бути переініціалізовані, а <b>"const"</b> - ні.</p>')
+document.write('<p>Декларація створює змінну без присвоєння значення, а ініціалізація надає їй значення.</p>')
